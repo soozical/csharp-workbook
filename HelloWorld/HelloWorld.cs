e13556a1973a86b46069dbf2e7735a2fe3146ce8
Susie <susie.odneal@gmail.com>
2018-10-01 20:32:25 -0500

    customized Hello World to Homework

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -4,9 +4,23 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello Sooz!");
+        static void Main(string[] args) {
+           string name = " ";
+           int age;
+           string nameOfPet = " ";
+
+        Console.WriteLine("Hi! What's your name?");
+        name = Console.ReadLine();
+        Console.WriteLine("Nice name. How old are you?");
+        age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Cool. I was born on October 1st, 2018.");
+        Console.WriteLine("What's the name of your pet?");
+        nameOfPet = Console.ReadLine();
+
+        Console.WriteLine("Cute!");
+        Console.WriteLine("Your name is {0}, you are {1} years old, and your pet's name is {2}!", name, age, nameOfPet);
+
         }
+        
     }
 }
