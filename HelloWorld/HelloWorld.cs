d3083ec1196baa282e4f6ce28823a05d5461a99c
Susie <susie.odneal@gmail.com>
2018-10-01 19:43:26 -0500

    added nickname instead of legal name

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Susie!");
+            Console.WriteLine("Hello Sooz!");
         }
     }
 }
