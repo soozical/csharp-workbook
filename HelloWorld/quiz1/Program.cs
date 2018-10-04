573e1297873995a59c1dbe99a59eda98e996215d
Susie <susie.odneal@gmail.com>
2018-10-03 20:55:26 -0500

    Added Quiz 1 biz

---
+++ HelloWorld/quiz1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace quiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Quiz 1");
+        }
+    }
+}
