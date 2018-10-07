30e055d9667199530f4cb992e45c02cd509fd256
Susie <susie.odneal@gmail.com>
2018-10-07 10:49:55 -0500

    fixed most of practice, converting text game

--- TextGame/Program.cs
+++ TextGame/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
-
+using System.Threading;
 namespace TextGame
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello stuff!");
+            int stick = 0;
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("Welcome to the cavern of secrets!");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Thread.Sleep(3000);
+            Console.WriteLine("You enter a dark cavern out of curiosity.");
+            Console.WriteLine("It is dark and you can only make out a small stick on the floor.");
+            Console.WriteLine("Do you take it? [Y/N]");
+            string ch1 = Console.ReadLine();
+            if(ch1 = 'Y')
+            {
+                Console.WriteLine("You have taken the stick!");
+                stick = 1;
+                Thread.Sleep(3000);
+            } else {
+                Console.WriteLine("You did not take the stick.");
+                stick = 0;
+
+            }
+            Console.WriteLine("As you proceed further into the cave, you see a small glowing object.");
+            Console.WriteLine("Do you approach the object? [Y/N]");
+            string ch2 = Console.ReadLine();
+            if (ch2 = 'Y')
+            {
+                Console.WriteLine("You approach the object...");
+                Thread.Sleep(200);
+                Console.WriteLine("The eye belongs to a giant spider!");
+                Console.WriteLine("Do you try and fight it? [Y/N]");
+                string ch3 = Console.ReadLine();
+                if (ch3 = 'Y')
+                {
+                    if (stick > 0)
+                    {
+                        Console.WriteLine("You only have a stick to fight with!");
+                        Console.WriteLine("You quickly jab the spider in it's eye and gain an advantage!");
+                        Thread.Sleep(2000);
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        Console.WriteLine("                  Fighting...                   ");
+                        Console.WriteLine("   YOU MUST HIT ABOVE A 5 TO KILL THE SPIDER    ");
+                        Console.WriteLine("IF THE SPIDER HITS HIGHER THAN YOU, YOU WILL DIE");
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    }
+                }
+            }
         }
     }
 }
--- week1-practice/Program.cs
+++ week1-practice/Program.cs
@@ -1,12 +1,94 @@
 ﻿using System;
+using System.Threading;
 
 namespace week1_practice
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            addition();
+            Thread.Sleep(1000);
+            yardConversion();
+            Thread.Sleep(1000);
+            datatypes();
+            Thread.Sleep(1000);
+            personalInfo();
+            Thread.Sleep(1000);
+            //numConversion();
+            Thread.Sleep(1000);
+            mathProbs();
         }
+        static void addition()
+        {
+            int x;
+            int y;
+            string placeholderX;
+            string placeholderY;
+            Console.WriteLine("Please enter an integer.");
+            placeholderX = Console.ReadLine();
+            x = int.Parse(placeholderX);
+            Console.WriteLine("Please enter another integer.");
+            placeholderY = Console.ReadLine();
+            y = int.Parse(placeholderY);
+            int z = x + y;
+            Console.WriteLine("The sum of your integers is {0}.", z);
+        }
+        static void yardConversion()
+        {
+            float yards;
+            float inches;
+            string placeholderYards;
+            //string placeholderInches;
+            Console.WriteLine("Let's convert some yards to inches.");
+            Thread.Sleep(1000);
+            Console.WriteLine("Enter some yards.");
+            Console.WriteLine("No need to enter a unit of measurement.");
+            placeholderYards = Console.ReadLine();
+            yards = float.Parse(placeholderYards);
+            Console.WriteLine(yards * 36);
+
+        }
+        static void datatypes()
+        {
+            bool people = true;
+            bool f = false;
+            float num = 5.5f;
+            Console.WriteLine(num * num);
+
+        }
+
+        static void personalInfo()
+        {
+            string firstName = "Susie";
+            string lastName = "Odneal";
+            byte age = 31;
+            string favoriteBand  = "Elohim";
+            string favoriteSports  = "Astros";
+            Console.WriteLine("Hi, my name is {0}{1}.", firstName, " ", lastName);
+            Console.WriteLine("I don't particularly like sports but I guess my favorite team is the {0}.", favoriteSports);
+
+
+
+        }   
+        /*static void numConversion()
+        {
+            var num = 3;
+            int myInt = int.Parse(num);
+            Console.WriteLine(myInt.GetType());
+
+        }*/
+
+        static void mathProbs()
+        {
+            float x = 10f;
+            float y = 100f;
+            Console.WriteLine(x + y);
+            Console.WriteLine(x - y);
+            Console.WriteLine(x * y);
+            Console.WriteLine(x / y);
+
+        }
+
     }
 }
