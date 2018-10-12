42551184491c2796ef45f0da0e3b2bfd0c5962d7
Susie <susie.odneal@gmail.com>
2018-10-12 13:08:34 -0500

    finished Rock Paper Scissors

--- RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors.cs
@@ -18,7 +18,48 @@ namespace RockPaperScissors
         
         public static string CompareHands(string hand1, string hand2)
         {
-            // Your code here
+            //Tie branch
+            if (hand1 == hand2)
+            {
+                Console.WriteLine("It's a tie!");
+            }
+            if (hand1 == "rock")
+            {
+                if(hand2 == "scissors")
+                {
+                    Console.WriteLine("Hand one wins!");
+                }
+                else if(hand2 == "paper")
+                {
+                    Console.WriteLine("Hand two wins!");
+                    //Console.WriteLine("Debug");
+                }
+            }
+            //Paper branch
+            if(hand1 =="paper")
+            {
+                if(hand2 == "rock")
+                {
+                    Console.WriteLine("Hand one wins!");
+                }
+                else if(hand2 == "scissors")
+                {
+                    Console.WriteLine("Hand two wins!");
+                }
+            }
+            //Scissors branch
+            if (hand1 == "scissors")
+            {
+                if(hand2 == "paper")
+                {
+                    Console.WriteLine("Hand one wins!");
+                }
+                else if(hand2 == "rock")
+                {
+                    Console.WriteLine("Hand two wins!");
+                }
+            }
+            
             return hand1 + ' ' + hand2;
         }
     }
