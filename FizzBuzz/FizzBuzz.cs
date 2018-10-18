fc0773dfca90799d1204ca93bcdc27c88452d31e
Susie <susie.odneal@gmail.com>
2018-10-18 08:26:58 -0500

    started work on fizzbuzz. Not all code paths return value error atm

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -6,7 +6,25 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a Number to Fizz Buzz");
+            int input = Convert.ToInt32(Console.ReadLine());
+            calcFizz(input);
+            
         }
+
+        static string calcFizz(int input) {
+            string fizz = "Fizz";
+            for(int i = 0; i < input; i++) {
+                if(input % 3 == 0){
+                    return fizz;
+                } else {
+                    input++;
+                    string  inputStr = Convert.ToString(input);
+                    return inputStr;
+                }
+                i++;
+            }
+        }    
+
     }
 }
