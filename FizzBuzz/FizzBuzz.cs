7b0e3a9d374068a5a29f6f45fdc3d05d76a82bb8
Susie <susie.odneal@gmail.com>
2018-10-18 17:34:35 -0500

    FizzBuzz working.

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -7,24 +7,30 @@ namespace FizzBuzz
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a Number to Fizz Buzz");
-            int input = Convert.ToInt32(Console.ReadLine());
-            calcFizz(input);
+             int input = Convert.ToInt32(Console.ReadLine());
             
+                for (int i = 1; i <= input; i++)
+                {
+                    if(i % 3 == 0 && i % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    } else if (i % 3 == 0)
+                    {
+                        Console.WriteLine("Fizz");
+                    } else if (i % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    } else 
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+
+            }
         }
 
-        static string calcFizz(int input) {
-            string fizz = "Fizz";
-            for(int i = 0; i < input; i++) {
-                if(input % 3 == 0){
-                    return fizz;
-                } else {
-                    input++;
-                    string  inputStr = Convert.ToString(input);
-                    return inputStr;
-                }
-                i++;
-            }
-        }    
 
     }
-}
+
+
