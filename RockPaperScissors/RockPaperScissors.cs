1d58e337902ac4748f948f6558b0985d5f3beff1
Susie <susie.odneal@gmail.com>
2018-10-22 19:58:53 -0500

    pushed up completed rock paper scissors program

--- RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors.cs
@@ -6,19 +6,83 @@ namespace RockPaperScissors
     {
         public static void Main()
         {
+            //string hand2 ="";
             Console.WriteLine("Enter hand 1:");
-            string hand1 = Console.ReadLine().ToLower();
-            Console.WriteLine("Enter hand 2:");
-            string hand2 = Console.ReadLine().ToLower();
+            string hand1 = Console.ReadLine();
+            Console.WriteLine("Choosing hand 2...");
+            ComputerChoice();
+            
+            string hand2 = ComputerChoice();
             Console.WriteLine(CompareHands(hand1, hand2));
 
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
         }
         
+        public static string ComputerChoice(){
+            string hand2 = "";
+            Random r = new Random();
+            int hand2Choice = r.Next(0, 2);
+
+            if(hand2Choice == 0)
+            {
+                return "rock";
+            }
+            if (hand2Choice == 1)
+            {
+                return "paper";
+            }
+            if(hand2Choice == 2)
+            {
+                return "scissors";
+            }
+            else return "That was not a valid response.";
+        }
+
         public static string CompareHands(string hand1, string hand2)
         {
-            // Your code here
+            //Tie branch
+            if (hand1 == hand2)
+            {
+                Console.WriteLine("It's a tie!");
+            }
+            if (hand1 == "rock")
+            {
+                if(hand2 == "scissors")
+                {
+                    Console.WriteLine("Hand one wins!");
+                }
+                else if(hand2 == "paper")
+                {
+                    Console.WriteLine("Hand two wins!");
+                    //Console.WriteLine("Debug");
+                }
+            }
+            //Paper branch
+            if(hand1 =="paper")
+            {
+                if(hand2 == "rock")
+                {
+                    Console.WriteLine("Hand one wins!");
+                }
+                else if(hand2 == "scissors")
+                {
+                    Console.WriteLine("Hand two wins!");
+                }
+            }
+            //Scissors branch
+            if (hand1 == "scissors")
+            {
+                if(hand2 == "paper")
+                {
+                    Console.WriteLine("Hand one wins!");
+                }
+                else if(hand2 == "rock")
+                {
+                    Console.WriteLine("Hand two wins!");
+                }
+            }
+            
             return hand1 + ' ' + hand2;
         }
     }
