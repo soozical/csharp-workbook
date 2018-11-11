4e200d9794bf5cb9b808d99aef2b7a84269237e7
Susie <susie.odneal@gmail.com>
2018-11-11 15:28:27 -0600

    Started towers

--- TowersOfHanoi/TowersOfHanoi.cs
+++ TowersOfHanoi/TowersOfHanoi.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace TowersOfHanoi
 {
@@ -6,7 +8,70 @@ namespace TowersOfHanoi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Game newGame = new Game();
+
         }
     }
+
+    class Game
+    {
+        Dictionary<char, Tower> gameMaster = new Dictionary<char, Tower>();
+        public Game ()
+        {
+            /* Tower A = new Tower();
+            Tower B = new Tower();
+            Tower C = new Tower();*/
+
+           Stack<Block> towerA = new Stack<Block>();
+           Stack<Block> towerB = new Stack<Block>();
+           Stack<Block> towerC = new Stack<Block>();
+
+           Block block1 = new Block(1);
+           Block block2 = new Block(2);
+           Block block3 = new Block(3);
+           Block block4 = new Block(4);
+
+           towerA.Push(block4);
+           towerA.Push(block3);
+           towerA.Push(block2);
+           towerA.Push(block1);
+
+            gameMaster.Add('A', towerA);
+            gameMaster.Add('B', towerB);
+            gameMaster.Add('C', towerC);
+
+        }
+
+        public Dictionary<Tower, Stack> DrawBoard()
+        {
+            Console.WriteLine(gameMaster);
+            return;
+        }
+
+    }
+
+    class Tower
+    {
+        Stack<Block> blocks = new Stack<Block>();
+    }   
+
+    class Block
+    {
+        public Block(int length)
+        {
+            this.length = length;
+        }
+
+        public int length
+        {
+            get;
+            private set;
+        }
+
+    }
+
+
+
 }
+
+
