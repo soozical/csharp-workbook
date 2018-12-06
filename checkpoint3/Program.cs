ff09d7f2248acd8bee39cc577dcfecc14862b37a
Susie <susie.odneal@gmail.com>
2018-12-05 19:31:55 -0600

    beginnings of Task class

---
+++ checkpoint3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace checkpoint3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+
+
+
+
+    }
+
+    class Task
+    {
+        public string title;
+        public string desc;
+
+        public int id{get; private set;}
+        bool isDone;
+        public Task(string title, string desc)
+        {
+            this.title = title;
+            this.desc = desc;
+            this.id = id;
+            
+            bool isDone = false;
+
+        }
+        
+
+
+    }
+
+
+
+}
