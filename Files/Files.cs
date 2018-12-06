637f4db4ce5e40dea275360e87a5ede06da57340
Susie <susie.odneal@gmail.com>
2018-12-05 18:23:42 -0600

    Working on displaying magic word/blanks

--- Files/Files.cs
+++ Files/Files.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+
 
 namespace Files
 {
@@ -6,7 +9,91 @@ namespace Files
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string magicWord = GetWord();
+            //Debug getting word
+            Console.WriteLine(magicWord);
+
+            int guesses = Game(magicWord);
+            Guessing(magicWord, guesses);
+
         }
+
+
+        //generate magic word from file
+        static string GetWord()
+        {
+            //File location, and read all lines into the program
+            String file = @"words_alpha.txt";
+            String[] words = File.ReadAllLines(file);
+
+            //generate random int index and find it in the array
+            Random rnd = new Random();
+            int index = rnd.Next(1, (words.Length - 1));
+
+            //return the string representation of index
+            String wordIndex = words[index];
+            return wordIndex;
+        }
+
+        static int Game(String magicWord)
+        {
+            //converst the letters into an array so we can count them
+            char[] letters = magicWord.ToCharArray();
+            int guesses = letters.Length + 4;
+            //tell the user the rules of the game
+            Console.WriteLine("Welcome to Hangman!");
+            Console.WriteLine();
+            Console.WriteLine("The word has " + letters.Length + " letters and you have " + guesses + " guesses.");
+            return guesses;
+        }
+
+        static void Guessing(String magicWord, int guesses)
+        {
+            char[] letters = magicWord.ToCharArray();
+            //while we still have guesses left...
+            while (guesses > 0)
+            {
+                //keep asking for guesses
+                Console.WriteLine();
+                Console.WriteLine("Enter your guess.");
+                string guess = Console.ReadLine();
+                if (magicWord.Contains(guess))
+                {
+                    Console.WriteLine("Correct!");
+                    
+                    for(int i = 0; i < magicWord.Length; i++)
+                    {
+
+                        if(guess[i] == magicWord[i])
+                        {
+                            Console.WriteLine(guess);
+                        }
+                        else
+                        {
+                            Console.WriteLine("_");
+                        }
+                    }
+
+
+
+                }
+                //if wrong, you get one less guess
+                else
+                {
+                    guesses--;
+                    Console.WriteLine("Incorrect. You have " + guesses + " remaining.");
+                }
+
+            }
+            //Game over.
+            if(guesses == 0 )
+            {
+                Console.WriteLine("Sorry, game over.");
+                Console.WriteLine();
+            }
+
+
+        }
+
     }
 }
